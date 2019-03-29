98b53158fa186fd394cdaf962f5c942f84d5694e
maxvoid <max4eva@gmail.com>
2019-03-29 00:27:08 -0400

    Massive push

--- assets/js/patatap.js
+++ assets/js/patatap.js
@@ -1,12 +1,12 @@
 // waits until the page is loaded before running any of the code within
 // $( document ).ready(function() {
-    // code
-    // code
-    // code
+	// code
+	// code
+	// code
 // });
 
 // shorthand for the above
 $(function(){
-    console.log("JS FILE CONNECTED");
+	console.log("JS FILE CONNECTED");
 
 });
